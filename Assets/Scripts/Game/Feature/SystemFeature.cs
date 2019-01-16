aa46c874a0e11e0c8837d98132f4fe8e607283ff
Andy <1174200070@qq.com>
2019-01-16 17:34:55 +0800

    添加系统初始化代码

--- Assets/Scripts/Game/Feature/SystemFeature.cs
+++ Assets/Scripts/Game/Feature/SystemFeature.cs
@@ -6,7 +6,17 @@ namespace Game
     {
         public SystemFeature(Contexts contexts) : base("System")
         {
+            AddInputSystem(contexts);
+        }
 
+        private void AddInputSystem(Contexts contexts)
+        {
+            Add(new InputUpButtonSystem(contexts));
+            Add(new InputDownButtonSystem(contexts));
+            Add(new InputLeftButtonSystem(contexts));
+            Add(new InputRightButtonSystem(contexts));
+            Add(new InputAttackOButtonSystem(contexts));
+            Add(new InputAttackXButtonSystem(contexts));
         }
     }
 }
